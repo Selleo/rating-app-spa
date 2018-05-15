8cc946c06e74ef68900b7b302e443401b0618340
Skysplit <manetamajster@gmail.com>
2018-05-15 09:47:24 +0200

    Setup react-router with redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import store from './store';
+import history from './history';
+import App from './App';
+
+import './index.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
